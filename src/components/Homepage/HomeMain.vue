4146d006e58c2f370b7524cb236cd6034cf8c820
MargheritaMottana <margherita.mottana@gmail.com>
2024-11-25 20:57:47 +0100

    -installato axios -implementato ciclo restaurants

--- src/components/Homepage/HomeMain.vue
+++ src/components/Homepage/HomeMain.vue
@@ -1,28 +1,39 @@
 <script>
-  //store
-      import { store } from '../../store.js';
+    //store
+        import { store } from '../../store.js';
 
-  /* 
-    Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
-    1) Importazione del componente
-    2) Dichiarazione del componente
-    3) Utilizzo del componente
-  */
-  // 1) Importazione del componente
+    // axios
+    import axios from 'axios';
 
   export default {
     data() {
       return { 
+        restaurants:[],
         store,
         src: 'https://img.freepik.com/foto-gratuito/casseruola-deliziosa-su-un-supporto-di-legno_140725-949.jpg?t=st=1732115219~exp=1732118819~hmac=a770c4e44b39756dc35b8e695723d986b23128123c95a768703d5215ea5b9dfe&w=1380'
       }
     },
-    // 2) Dichiarazione del componente
-    components: {
-
+    mounted(){
+        this.getRestaurants();
+        // this.getRestaurantSlug();
     },
     methods: {
-      
+    //   getRestaurantSlug(){
+    //     axios
+    //         .get('http://127.0.0.1:8000/api/restaurants' + '/' + this.slug)
+    //         .then(res => {
+    //             this.restaurants = res.data.data.restaurants.data;
+    //             console.log(this.slug)
+    //         });
+    //   }, 
+      getRestaurants(){
+        axios
+            .get('http://127.0.0.1:8000/api/restaurants')
+            .then(res => {
+                this.restaurants = res.data.data.restaurants.data;
+                console.log(this.restaurants)
+            });
+      },
     }
   }
 </script>
@@ -33,20 +44,36 @@
    
     <main id="main">
 
+        <!-- <button>
+            <a class="nav-link" :href="getRestaurantSlug()">
+                <i class="fa-solid fa-house text-warning me-2"></i>
+                vai alla tua dashboard
+            </a>
+        </button> -->
+
         <div class="container mt-5">
 
           <div class="row">
 
-            <div class="col-3 mb-4" v-for="(article, i) in store.articlesInfo" :key="i">
+            <div class="col-3 mb-4 d-flex" v-for="restaurant in restaurants" :key="restaurant.id">
 
               <!-- shadow-sm -->
-              <div class="card rounded-top-5 p-2 ">
-                <img class="card-img-top rounded rounded-top-5" :src="src" :alt="article.title">
-                <!-- <img :src="article.photo" class="card-img-top" :alt="article.title"> -->
+              <div class="card rounded-top-5 p-2 align-self-stretch">
+                <!-- <img class="card-img-top rounded rounded-top-5" :src="src" :alt="article.title"> -->
+                <img class="card-img-top rounded rounded-top-5" :src="restaurant.img" :alt="restaurant.restaurant_name">
 
                 <div class="card-body text-center">
-                  <h6 class="card-title mb-3">{{ article.title }}</h6>
-                  <a href="#" class="btn btn-outline-warning text-black rounded-pill">Menu</a>
+                    <h6 class="card-title mb-3">{{ restaurant.restaurant_name }}</h6>
+
+                    <div class="mb-3">
+                        <span v-for="typology in restaurant.typologies" :key="typology.id" class="badge rounded-pill text-bg-warning mb-2 mx-2">
+                            {{ typology.typology_name }}
+                        </span>
+                    </div>
+
+                    <a href="#" class="btn btn-outline-warning text-black rounded-pill">
+                        Menu
+                    </a>
                 </div>
 
               </div>
