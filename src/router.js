362d1bbcacc3d7919d24de16c52b20219d25e397
Eduard Condurache <eduardcondurache96@gmail.com>
2024-11-28 22:59:26 +0100

    Creato pagine checkout che manda dati al backend/database con la sua rotta

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import RestaurantsPage from './pages/RestaurantsPage.vue';
 import NotFoundPage from './pages/NotFoundpage.vue'
 import SingleRestaurantPage from './pages/SingleRestaurantPage.vue'
+import Checkout from './pages/Checkout.vue'
 
 
 const router = createRouter({
@@ -17,6 +18,11 @@ const router = createRouter({
             path: '/restaurant/:slug',
             name: 'restaurant-show',
             component: SingleRestaurantPage
+        },
+        {
+            path: '/checkout',
+            name: 'checkout',
+            component: Checkout
         },
         {
             path: '/:pathMatch(.*)*',
