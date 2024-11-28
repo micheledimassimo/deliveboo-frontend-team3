28f73f250c17e479153f16400ac010cc9b8e2ad4
MargheritaMottana <margherita.mottana@gmail.com>
2024-11-28 17:09:58 +0100

    -aggiunta animazione bottone carrello

--- src/pages/SingleRestaurantPage.vue
+++ src/pages/SingleRestaurantPage.vue
@@ -11,7 +11,7 @@
                     restaurant: null,
                     src: 'https://img.freepik.com/foto-gratuito/casseruola-deliziosa-su-un-supporto-di-legno_140725-949.jpg?t=st=1732115219~exp=1732118819~hmac=a770c4e44b39756dc35b8e695723d986b23128123c95a768703d5215ea5b9dfe&w=1380',
                     is_visible: true,
-                    cart: []
+                    cart: [],
                 }
         },
         components: {
@@ -42,6 +42,8 @@
                             } else {
                                 menu_item.image = this.src;
                             }
+                        menu_item.isAdded = false;
+
                     });
                 });
             },
@@ -56,6 +58,14 @@
                     // Aggiungi il piatto al carrello con quantità iniziale 1
                     this.cart.push({ ...menu_item, quantity: 1 });
                 }
+
+                // Attiva l'animazione solo per il pulsante specifico
+                menu_item.isAdded = true;
+                setTimeout(() => {
+                    menu_item.isAdded = false;  // Ripristina lo stato dopo l'animazione
+                }, 2000);                       // Durata dell'animazione (2 secondi)
+
+                // Salva il carrello alla fine di questa operazione
                 this.saveCart();
             },
             increaseQuantity(item) {
@@ -87,7 +97,7 @@
                 if (savedCart) {
                     this.cart = JSON.parse(savedCart);
                 }
-            },   
+            },
         }
     }
 </script>
@@ -130,6 +140,7 @@
                                     </small>
 
                                 </div>
+
                                 <div class="d-flex align-items-center">
                                     
                                     <button class="btn btn-sm btn-outline-warning me-2 quantity-button" @click="decreaseQuantity(item)">-</button>
@@ -168,8 +179,6 @@
 
                     <img :src="menu_item.image" class="rounded-circle border border-dark-subtle border-1 shadow-sm" :alt="menu_item.item_name">
 
-                    <!-- <img class="card-img-top rounded rounded-top-5" :src="menu_item.image" :alt="menu_item.item_name"> -->
-
                     <div class="card-body text-center">
                         <h6 class="card-title mb-2 fw-bold">{{ menu_item.item_name }}</h6>
                         <p class="mb-2">{{ menu_item.description }}</p>
@@ -179,9 +188,20 @@
                             {{ menu_item.price }} € 
                         </div>
 
-                        <button class="btn btn-outline-warning text-black rounded-pill" @click="addToCart(menu_item)">
-                            Aggiungi al carrello
+                        <button class="add-to-cart"
+                            @click="addToCart(menu_item)"
+                            :class="{ 'add-to-cart': true, 'added': menu_item.isAdded }"
+                        >
+                            <div class="default">Aggiungi</div>
+                            <div class="cart">
+                                <div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                            <div class="dots"></div>
                         </button>
+
                     </div>
 
                 </div>
@@ -194,29 +214,26 @@
 </template>
 
 <style lang="scss" scoped>
-//  @use '../../assets/scss/main.scss' as *;
-@use '../assets/scss/main.scss' as *;
-  // Import all of Bootstrap's CSS
-  @import "bootstrap/scss/bootstrap";
+    //  @use '../../assets/scss/main.scss' as *;
+    @use '../assets/scss/main.scss' as *;
+    // Import all of Bootstrap's CSS
+    @import "bootstrap/scss/bootstrap";
 
-  h1{
+    h1{
     margin-bottom: 100px;
-  }
-
-  .col-md-6{
+    }
+    .col-md-6{
     .card{
         margin-bottom: 80px;
     }
-  }
-
-  .quantity-button{
+    }
+    .quantity-button{
     width: 25px;
     height: 25px;
     border-radius: 100%;
     line-height: 10px;
-  }
-
-  .card{
+    }
+    .card{
     box-shadow: 0 0 5px rgba(14, 14, 14, 0.2); 
     transition: box-shadow .3s;
     position: relative;
@@ -237,10 +254,250 @@
         margin-top: 80px;
     }
 
-  }
-   #menuitem:hover{
+    }
+    #menuitem:hover{
     border: 1px solid rgb(255, 206, 30);
     box-shadow: 0 5px 20px rgba(0, 0, 0, 0.503); 
-  }
- 
+    }
+
+    // cart
+    .add-to-cart {
+        --color: #000000;
+        --icon: var(--color);
+        --cart: #000000;
+        --dots: #000000;
+        --background: #ffc107;
+        --shadow: #{rgba(#00093D, .16)};
+        width: 157.47px;
+        height: 42.68px;
+
+        cursor: pointer;
+
+        position: relative;
+        outline: none;
+        border: none;
+        -webkit-appearance: none;
+        -webkit-tap-highlight-color: transparent;
+        font-size: 16px;
+        border-radius: 22px;
+        padding: 12px 32px;
+        font-weight: 500;
+        line-height: 20px;
+        transform: scale(var(--s, .97));
+        box-shadow: 0 var(--s-y, 4px) var(--s-b, 12px) var(--shadow);
+        color: var(--color);
+        background: var(--background);
+        transition: transform .3s, box-shadow .3s;
+
+        .default {
+            padding-left: 16px;
+            position: relative;
+            opacity: var(--o, 1);
+            transform: scale(var(--s, 1));
+            transition: transform .3s, opacity .3s;
+            transition-delay: var(--d, .3s);
+            &:before,
+            &:after {
+                content: '';
+                width: 2px;
+                height: 12px;
+                left: 0;
+                top: 4px;
+                border-radius: 1px;
+                background: var(--icon);
+                position: absolute;
+                transform: rotate(var(--r, 0deg));
+                transition: transform .25s;
+            }
+            &:after {
+                --r: 90deg;
+            }
+        }
+        .success {
+            opacity: var(--o, 0);
+            transform: translate(-50%, var(--y, 12px));
+            position: absolute;
+            top: 12px;
+            left: 50%;
+            transition: opacity .3s, transform .3s;
+            transition-delay: var(--d, 0s);
+        }
+        .dots {
+            width: 4px;
+            height: 4px;
+            top: 20px;
+            left: 50%;
+            margin-left: -7px;
+            border-radius: 2px;
+            position: absolute;
+            transform-origin: 10px 50%;
+            background: var(--dots);
+            box-shadow: 5px 0 0 var(--dots), 10px 0 0 var(--dots);
+            opacity: var(--o, 0);
+            transform: scale(var(--s, .4));
+            transition: opacity .3s, transform .3s;
+            transition-delay: var(--d, 0s);
+        }
+        .cart {
+            position: absolute;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            z-index: 1;
+            border-radius: inherit;
+            overflow: hidden;
+            -webkit-mask-image: -webkit-radial-gradient(white, black);
+            &:before {
+                content: '';
+                position: absolute;
+                width: 20px;
+                height: 16px;
+                background: var(--background);
+                top: 17px;
+                right: 100%;
+                z-index: 1;
+                margin-right: -20px;
+                transform: translateX(-18px) rotate(-16deg);
+            }
+            & > div {
+                top: 13px;
+                right: 100%;
+                transform: translateX(-18px) rotate(-16deg);
+                position: absolute;
+                z-index: 2;
+                transform-origin: 1px 21px;
+                &:before,
+                &:after {
+                    content: '';
+                    position: absolute;
+                    top: var(--t, 4px);
+                    left: var(--l, 0);
+                    height: 2px;
+                    width: var(--w, 18px);
+                    background: var(--cart);
+                    border-radius: 1px;
+                }
+                &:after {
+                    --w: 16px;
+                    --t: 14px;
+                    --l: 1px;
+                }
+                & > div {
+                    width: 2px;
+                    height: var(--h, 15px);
+                    border-radius: 1px;
+                    transform: rotate(var(--r, -8deg));
+                    background: var(--cart);
+                    position: relative;
+                    &:before,
+                    &:after {
+                        content: '';
+                        position: absolute;
+                        background: inherit;
+                    }
+                    &:after {
+                        width: 4px;
+                        height: 4px;
+                        border-radius: 2px;
+                        bottom: var(--b, -6px);
+                        left: var(--l, 0);
+                    }
+                    &:first-child {
+                        &:before {
+                            border-radius: inherit;
+                            top: 0;
+                            right: 0;
+                            height: 2px;
+                            width: 6px;
+                            transform-origin: 5px 1px;
+                            transform: rotate(16deg);
+                        }
+                    }
+                    &:last-child {
+                        --h: 12px;
+                        --r: 8deg;
+                        position: absolute;
+                        left: 16px;
+                        bottom: -1px;
+                        &:after {
+                            --l: -2px;
+                            --b: -5px;
+                        }
+                    }
+                }
+            }
+        }
+        &.added {
+            .default {
+                --o: 0;
+                --s: .8;
+                --d: 0s;
+                &:before {
+                    --r: -180deg;
+                }
+                &:after {
+                    --r: -90deg;
+                }
+            }
+            .dots {
+                --o: 1;
+                --s: 1;
+                --d: .3s;
+                animation: dots 2s linear forwards;
+            }
+            .success {
+                --o: 1;
+                --y: 0;
+                --d: 1.8s;
+            }
+            .cart {
+                &:before,
+                & > div {
+                    animation: cart 2s forwards;
+                }
+            }
+        }
+    }
+    @keyframes cart {
+        41%,
+        49%,
+        57%,
+        60% {
+            transform: translateX(72px) rotate(0deg);
+        }
+        40%,
+        47%,
+        54% {
+            transform: translateX(72px) rotate(0deg) translateY(1px);
+        }
+        100% {
+            transform: translateX(180px) rotate(-16deg);
+        }
+    }
+    @keyframes dots {
+        5% {
+            transform: translateY(0);
+        }
+        25% {
+            transform: translateY(-20px) rotate(-32deg);
+            box-shadow: 5px 0 0 var(--dots), 10px 0 0 var(--dots);
+        }
+        31% {
+            box-shadow: 5px -4px 0 var(--dots), 10px -8px 0 var(--dots);
+        }
+        32%,
+        50% {
+            transform: translateY(0) rotate(0deg);
+            opacity: 1;
+        }
+        45%,
+        100% {
+            box-shadow: 5px 0 0 var(--dots), 10px 0 0 var(--dots);
+        }
+        51%,
+        100% {
+            opacity: 0;
+        }
+    } 
 </style>
